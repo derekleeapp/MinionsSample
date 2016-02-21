0e35f99c25478150b88d9d8087caef6f6cd964c2
Ewan Leaver <ewanleaver@gmail.com>
2016-02-21 10:37:50 +0900

    Replaced implementation of MinionsRepo to use MinionsHttpRequester

--- MinionsSample/MinionsRepo.swift
+++ MinionsSample/MinionsRepo.swift
@@ -8,28 +8,17 @@ struct MinionsRepo
     <P: DataParser where P.ParsedObject == Minion>
 {
     let parser: P
+    let requester: MinionsHttpRequester
 }
 
 extension MinionsRepo: Repo {
     func getMinionsAsynchronously(completion: ([Minion] -> Void)) {
 
         let urlString = "http://jsonplaceholder.typicode.com/users"
-        let url = NSURL(string: urlString)!
-        let urlRequest = NSURLRequest(URL: url)
-
-        let _ = NSURLSession
-            .sharedSession()
-            .dataTaskWithRequest(
-                urlRequest,
-                completionHandler: {
-                    (maybeData: NSData?,
-                    maybeResponse: NSURLResponse?,
-                    maybeError: NSError?) -> Void in
-
-                    let minionsArray = self.parser.parse(maybeData!)
-
-                    completion(minionsArray)
-                })
-            .resume()
+        self.requester.request(urlString) { (data: NSData) -> Void in
+            let minionsArray = self.parser.parse(data)
+            
+            completion(minionsArray)
+        }
     }
 }
