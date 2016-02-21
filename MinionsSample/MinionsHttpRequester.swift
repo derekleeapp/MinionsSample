a55b4b9eccafb0da54281d77b355ba0c17a3b7ad
Ewan Leaver <ewanleaver@gmail.com>
2016-02-21 10:32:13 +0900

    Extracted MinionRepo's http requester into a request() method

--- MinionsSample/MinionsHttpRequester.swift
+++ MinionsSample/MinionsHttpRequester.swift
@@ -2,6 +2,23 @@ import Foundation
 
 class MinionsHttpRequester {
     
-    
-    
+    //func request(urlString: String) -> NSData {
+    func request(urlString: String, completion: (NSData -> Void)) {
+        
+        let url = NSURL(string: urlString)!
+        let urlRequest = NSURLRequest(URL: url)
+        
+        let _ = NSURLSession
+            .sharedSession()
+            .dataTaskWithRequest(
+                urlRequest,
+                completionHandler: {
+                    (maybeData: NSData?,
+                    maybeResponse: NSURLResponse?,
+                    maybeError: NSError?) -> Void in
+                    
+                    completion(maybeData!)
+            })
+            .resume()
+    }
 }
